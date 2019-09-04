74370d9d8e9345d68a14e6fbb130943eba6a4442
任小龙 <40367288@qq.com>
2019-09-04 18:00:24 +0800

    2

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
-
 require "vendor/autoload.php";
 
 
+$m = getParamM();
+$file = './test/' . trim($m, '/') . '/index.php';
+if (is_file($file)) {
+    t('start');
+    include $file;
+    t('over');
+} else {
+    p('没有找到目录文件 ' . $file);
+}
 
+function getParamM()
+{
+    $m = '';
+    if (isset($_GET['m'])) {
+        $m = $_GET['m'];
+    } else {
+        if (isset($_SERVER['argv'])) {
+            $m = $_SERVER['argv'][1];
+        }
+    }
+    return $m;
+}
